b2df0f06bd6735b42f2219733faed777b6a7c2f7
Peter Vertesi <vpeter1119@gmail.com>
2020-12-10 13:57:47 +0100

    Add character-view to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { AuthComponent } from './auth/auth.component';
+import { CharacterViewComponent } from './characters/character-view/character-view.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'auth', component: AuthComponent },
+  { path: 'character-view', component: CharacterViewComponent},
 ];
 
 @NgModule({
